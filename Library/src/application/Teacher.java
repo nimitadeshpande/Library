b3d9d8d9eedb7228b289db5226ff48d49ac3569b
Nimmu <Nimmu@idea-PC>
2018-02-13 08:16:53 -0500

    Project Library

---
+++ Library/src/application/Teacher.java
@@ -0,0 +1,32 @@
+package application;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Teacher extends Borrower {
+	public static int DAY_LIMIT, BOOK_LIMIT;
+	public static double FINE_PER_DAY;
+	
+	public Teacher(String id, String lastName, String firstName) {	
+		super();
+		this.id = new SimpleStringProperty(id);
+		this.lastName = new SimpleStringProperty(lastName);
+		this.firstName = new SimpleStringProperty(firstName);
+		bookIds = new ArrayList<SimpleStringProperty>();
+		this.borrowedDates = new ArrayList<LocalDate>();
+		this.returnDates = new ArrayList<LocalDate>();
+	}
+	
+	public Teacher(String id, String lastName, String firstName, ArrayList<SimpleStringProperty> bookIds,
+			ArrayList <LocalDate> borrDates, ArrayList <LocalDate> retDates) {
+		super();
+		this.id = new SimpleStringProperty(id);
+		this.lastName = new SimpleStringProperty(lastName);
+		this.firstName = new SimpleStringProperty(firstName);
+		this.bookIds = bookIds;
+		this.borrowedDates = borrDates;
+		this.returnDates = retDates;
+	}
+}
